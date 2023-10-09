252d4338f929899aa37bcf9c3646d028db05be43
Neylix <julien.leclerc05@protonmail.com>
2023-10-09 09:42:54 +0200

    Fix for indent_blankline 3

--- plugins/indent_blankline.lua
+++ plugins/indent_blankline.lua
@@ -1,7 +1,9 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  version = "3.2.1",
   opts = {
     indent = {char = '┊', smart_indent_cap = true},
-    whitespace = {remove_blankline_trail = false}
+    whitespace = {remove_blankline_trail = false},
+    scope = {enabled = false}
   }
 }
--- polish.lua
+++ polish.lua
@@ -1,7 +1,8 @@
 return function()
   -- Workaround for indent_blankline color
-  -- local whitespace_color = vim.fn.synIDattr(vim.fn.hlID('Whitespace'), 'fg')
-  -- vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = whitespace_color, nocombine = true })
+  local whitespace_color = vim.fn.synIDattr(vim.fn.hlID('Whitespace'), 'fg')
+  vim.api.nvim_set_hl(0, 'IblIndent', { fg = whitespace_color, nocombine = true })
+  vim.api.nvim_set_hl(0, 'IblWhitespace', { fg = whitespace_color, nocombine = true })
 
   -- Auto resize split when window os resized
   vim.api.nvim_create_autocmd('VimResized', { command = 'wincmd =' })
