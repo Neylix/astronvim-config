297c54cc366170a2999a35fdd027d5d3ed14b3c5
Neylix <julien.leclerc05@protonmail.com>
2023-11-01 21:30:24 +0100

    Fix Alpha landing page icons

--- plugins/alpha.lua
+++ plugins/alpha.lua
@@ -1,5 +1,6 @@
 return {
   "goolord/alpha-nvim",
+  cmd = "Alpha",
   opts = function()
     local dashboard = require "alpha.themes.dashboard"
     dashboard.section.header.val = {
@@ -16,23 +17,27 @@ return {
       "    ██   ████   ████   ██ ██      ██",
     }
     dashboard.section.header.opts.hl = "DashboardHeader"
+    dashboard.section.footer.opts.hl = "DashboardFooter"
 
-    local button = require("astronvim.utils").alpha_button
+    local button, get_icon = require("astronvim.utils").alpha_button, require("astronvim.utils").get_icon
     dashboard.section.buttons.val = {
-      button("LDR f f", "  Find File  "),
-      button("LDR f o", "  Recents  "),
-      button("LDR f w", "  Find Word  "),
-      button("LDR f n", "  New File  "),
+      button("LDR n    ", get_icon("FileNew", 2, true) .. "New File  "),
+      button("LDR f f  ", get_icon("Search", 2, true) .. "Find File  "),
+      button("LDR f o  ", get_icon("DefaultFile", 2, true) .. "Recents  "),
+      button("LDR f w  ", get_icon("WordFile", 2, true) .. "Find Word  "),
       button("LDR g o l", "  Octo pr list  "),
     }
 
-    dashboard.section.footer.val =
-    { " ", " ", " ", "AstroNvim loaded " .. require("lazy").stats().count .. " plugins " }
-    dashboard.section.footer.opts.hl = "DashboardFooter"
-
-    dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
-    dashboard.config.layout[3].val = 5
+    dashboard.config.layout = {
+      { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+      dashboard.section.header,
+      { type = "padding", val = 5 },
+      dashboard.section.buttons,
+      { type = "padding", val = 3 },
+      dashboard.section.footer,
+    }
     dashboard.config.opts.noautocmd = true
     return dashboard
-  end
+  end,
+  config = require "plugins.configs.alpha",
 }
