012315c6cf84b1570e93e796529c6273c2cade64
Neylix <julien.leclerc05@protonmail.com>
2024-01-24 11:02:15 +0100

    Get indent_blankline to last version

--- plugins/indent_blankline.lua
+++ plugins/indent_blankline.lua
@@ -1,6 +1,5 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  version = "3.2.1",
   opts = {
     indent = {char = '┊', smart_indent_cap = true},
     whitespace = {remove_blankline_trail = false},
